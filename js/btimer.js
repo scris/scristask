baf803c36e51e31513732455ad39b8a16e25780e
Dominik <slaaker@126.com>
2018-08-20 11:20:10 +0800

    notification

--- js/btimer.js
+++ js/btimer.js
@@ -1,45 +1,48 @@
-
 var player = $("#finishsound")[0];
-$("#starty").click(function(){
-    maxtime=$("#worktime").val() * 60;
-    if(!/^\d+$/.test(maxtime))
-    {
-            maxtime = 52 * 60;
-    }
-    player.load();
-    btimer = setInterval("workCountDown()", 1000);      
+$("#starty").click(function () {
+	maxtime = $("#worktime").val() * 60;
+	if (!/^\d+$/.test(maxtime)) {
+		maxtime = 52 * 60;
+	}
+	player.load();
+	btimer = setInterval("workCountDown()", 1000);
 });
-            function workCountDown() {
-                if (maxtime >= 0) {
-                    minutes = Math.floor(maxtime / 60);
-                    seconds = Math.floor(maxtime % 60);
-                    $("#minute").text("Worktime " +minutes);
-                    $("#second").text(seconds);
-                    if (maxtime == 5 * 60) alert("5 minutes left for working");
-                        --maxtime;
-                } else{
-                    player.play();
-                    clearInterval(btimer);
-                    alert("Worktime is up. Now let us have a break!☺️");
-                    maxtime=$("#breaktime").val() * 60;
-                    if(!/^\d+$/.test(maxtime))
-                    {
-                        maxtime = 17 * 60;
-                    }
-                    btimer = setInterval("breakCountDown()", 1000);  
-                }
-            }
-            function breakCountDown() {
-                if (maxtime >= 0) {
-                    minutes = Math.floor(maxtime / 60);
-                    seconds = Math.floor(maxtime % 60);
-                    $("#minute").text("Breaktime " + minutes);
-                    $("#second").text(seconds);
-                    --maxtime;
-                } else{
-                    player.play();
-                    clearInterval(btimer);
-                    alert("Now it is time for working.👍");
-                }
-            }
-            
+
+function workCountDown() {
+	if (maxtime >= 0) {
+		minutes = Math.floor(maxtime / 60);
+		seconds = Math.floor(maxtime % 60);
+		$("#minute").text("Worktime " + minutes);
+		$("#second").text(seconds);
+		if (maxtime == 5 * 60) {
+			alert("5 minutes left for working");
+			const notify3 = new Notification("5 minutes left for working");
+		}
+		--maxtime;
+	} else {
+		player.play();
+		clearInterval(btimer);
+		alert("Worktime is up. Now let us have a break!");
+		const notify4 = new Notification("Worktime is up. Now let us have a break!");
+		maxtime = $("#breaktime").val() * 60;
+		if (!/^\d+$/.test(maxtime)) {
+			maxtime = 17 * 60;
+		}
+		btimer = setInterval("breakCountDown()", 1000);
+	}
+}
+
+function breakCountDown() {
+	if (maxtime >= 0) {
+		minutes = Math.floor(maxtime / 60);
+		seconds = Math.floor(maxtime % 60);
+		$("#minute").text("Breaktime " + minutes);
+		$("#second").text(seconds);
+		--maxtime;
+	} else {
+		player.play();
+		clearInterval(btimer);
+		alert("Now it is time for working.");
+		const notify5 = new Notification("Now it is time for working.");
+	}
+}
--- js/ptimer.js
+++ js/ptimer.js
@@ -1,41 +1,44 @@
-
 var pplayer = $("#finishsound")[0];
-$("#pstart").click(function(){
-    pmaxtime=$("#ptime").val() * 60;
-    if(!/^\d+$/.test(pmaxtime))
-    {
-            pmaxtime = 52 * 60;
-    }
-    pplayer.load();
-    $("#ppause").show(700);
-    ptimer = setInterval("CountDown()", 1000);      
+$("#pstart").click(function () {
+	pmaxtime = $("#ptime").val() * 60;
+	if (!/^\d+$/.test(pmaxtime)) {
+		pmaxtime = 52 * 60;
+	}
+	pplayer.load();
+	$("#ppause").show(700);
+	ptimer = setInterval("CountDown()", 1000);
 });
-$("#ppause").click(function(){
-    if($("#ppause").val() == "Pause") pausyFunction();     
-    else resumyFunction();
+$("#ppause").click(function () {
+	if ($("#ppause").val() == "Pause") pausyFunction();
+	else resumyFunction();
 });
-            function CountDown() {
-                if (pmaxtime >= 0) {
-                    pminutes = Math.floor(pmaxtime / 60);
-                    pseconds = Math.floor(pmaxtime % 60);
-                    $("#pminute").text(pminutes);
-                    $("#psecond").text(pseconds);
-                    if (pmaxtime == 5 * 60) alert("5 minutes left");
-                        --pmaxtime;
-                } else{
-                    pplayer.play();
-                    clearInterval(ptimer);
-                    alert("Time is up.");
-                    $("#ppause").hide(700);
-                }
-            }
-            function pausyFunction()
-            {
-                $("#ppause").val("Resume");
-                clearInterval(ptimer);
-            }
-            function resumyFunction()
-            {
-                $("#ppause").val("Pause");
-                ptimer = setInterval("CountDown()", 1000);
-            }
+
+function CountDown() {
+	if (pmaxtime >= 0) {
+		pminutes = Math.floor(pmaxtime / 60);
+		pseconds = Math.floor(pmaxtime % 60);
+		$("#pminute").text(pminutes);
+		$("#psecond").text(pseconds);
+		if (pmaxtime == 5 * 60) {
+			alert("5 minutes left");
+			const notify1 = new Notification ("5 minutes left");
+		}
+		--pmaxtime;
+	} else {
+		pplayer.play();
+		clearInterval(ptimer);
+		alert("Time is up.");
+		const notify2 = new Notification ("Time is up.");
+		$("#ppause").hide(700);
+	}
+}
+
+function pausyFunction() {
+	$("#ppause").val("Resume");
+	clearInterval(ptimer);
+}
+
+function resumyFunction() {
+	$("#ppause").val("Pause");
+	ptimer = setInterval("CountDown()", 1000);
+}
