eee64df94dec78572ad81791f76ddac5c6abf7e0
Dominik Qiu <slaaker@126.com>
2018-11-25 08:32:53 +0800

    weekly updated

--- src/components/taskitem.vue
+++ src/components/taskitem.vue
@@ -5,7 +5,7 @@
         <span style="float: left;padding: 5px 0 12px 0">{{ todo.starttime }}<br> +{{ todo.lastfor }}</span>
       </el-aside>
       <el-main>
-        <span>{{ todo.title }}</span>
+        <span style="float: left">{{ todo.title }}</span>
         <el-button style="float: right; padding: 1px 0" type="text" @click="$emit('delete', todo.id)" class="el-icon-close"></el-button>
       </el-main>
     </el-container>      
