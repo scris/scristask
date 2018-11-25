814daa9bb4e06c0e71f4ea9941ff391c79ef868c
Dominik Qiu <slaaker@126.com>
2018-11-25 08:12:09 +0800

    Update taskitem.vue

--- src/components/taskitem.vue
+++ src/components/taskitem.vue
@@ -1,9 +1,14 @@
 <template>
     <el-card shadow="never">
-      <span style="float: left">{{ todo.title }}</span>
-      <el-button style="float: right; padding: 1px 0" type="text" @click="$emit('delete', todo.id)" class="el-icon-close"></el-button>
-      <br>
-      <span style="float: left;padding: 5px 0 12px 0">Start at: {{ todo.starttime }} Last for: {{ todo.lastfor }} minutes</span>
+    <el-container>
+      <el-aside width="200px">
+        <span style="float: left;padding: 5px 0 12px 0">{{ todo.starttime }}<br> +{{ todo.lastfor }}</span>
+      </el-aside>
+      <el-main>
+        <span style="float: left">{{ todo.title }}</span>
+        <el-button style="float: right; padding: 1px 0" type="text" @click="$emit('delete', todo.id)" class="el-icon-close"></el-button>
+      </el-main>
+    </el-container>      
     </el-card>
 </template>
 
